743ef4cbaec029c61ca46d417a3beb90c187d576
effilin <effienummer@gmail.com>
2024-06-07 19:40:28 -0400

    5.4done

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const app2 = express()
 const port = 80
 const port2 = 3000
 import calculatorRoutes from "./routes/calculatorRoutes.js";
+import friendRoutes from "./routes/friendRoute.js";
 
 app.use('/calculator', calculatorRoutes);
-
+app.use('/friends', friendRoutes);
 app.use('/', express.static('public'))
-
+app.use(express.json());
 
 
 app.listen(port, () => {
